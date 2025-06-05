b09bd845d01116646044ca5f783cf8e727a6682d
Anastasiia Carlini <anastasiia.carlini@gmail.com>
2025-06-06 00:03:06 +0300

    implemented fun-ty, of displ. tasks

--- script.js
+++ script.js
@@ -48,9 +48,23 @@ function deleteAllTasks() {
 }
 
 function displayTasks() {
-
+    todoList.innerHTML = "";
+    todo.forEach((item, index) => {
+        const p = document.createElement("p");
+        p.innerHTML = `
+            <div class="todo-container">
+                <input type="checkbox" class="todo-checkbox" id=""input-${index} ${item.disabled ? "checked" : ""}/>
+                <p id="todo-${index}" class="${item.disabled ? "disabled" : ""}" onclick="editTask(${index})"> ${item.text}
+                </p>
+            </div>
+        `;
+        p.querySelector(".todo-checkbox").addEventListener("change", () => {
+            toggleTask(index);
+        });
+        todoList.appendChild(p);
+    });
 }
 
 function saveToLocalStorage() {
-    
+    localStorage.setItem("todo", JSON.stringify(todo));
 }
