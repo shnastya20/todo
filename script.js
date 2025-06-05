cd8a32e55055484752e9a0ad2793321a2d847a32
Anastasiia Carlini <anastasiia.carlini@gmail.com>
2025-06-06 00:38:20 +0300

    implement edit task fun-ty

--- script.js
+++ script.js
@@ -63,6 +63,26 @@ function displayTasks() {
         });
         todoList.appendChild(p);
     });
+    todoCount.textContent = todo.length;
+}
+
+function editTask(index) {
+    const todoItem = document.getElementById(`todo-${index}`);
+    const existingText = todo[index].text;
+    const inputElement = document.createElement("input");
+
+    inputElement.value = existingText;
+    todoItem.replaceWith(inputElement);
+    inputElement.focus();
+
+    inputElement.addEventListener("blur", function() {
+        const updatedText = inputElement.validationMessage.trim();
+        if (updatedText) {
+            todo[index].text = updatedText;
+            saveToLocalStorage();
+        }
+        displayTasks();
+    });
 }
 
 function toggleTask(index) {
